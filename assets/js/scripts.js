99c71aad55f730b245636ae63be7255525aaf80f
Vladislav Soprun <develop@soprun.com>
2022-05-22 19:15:03 +0300

    Commit...

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -21,4 +21,9 @@
             hljs.highlightElement(el);
         });
     });
+
+    // var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    // var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    //     return new bootstrap.Tooltip(tooltipTriggerEl)
+    // })
 })(document);
