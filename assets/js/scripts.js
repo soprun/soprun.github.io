6852f0fa186e52b16715e5a2f74e17e1cb7d46e8
Vladislav Soprun <develop@soprun.com>
2022-05-23 15:29:22 +0300

    Commit...

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,23 +4,23 @@
     // document.addEventListener('DOMContentLoaded', (event) => {
     // });
 
-    document.querySelectorAll('link').forEach((link) => {
-        if (link.rel === "preload") {
-            console.debug(link.href);
-
-            link.rel = "stylesheet"
-            link.onload = null
-        }
-    });
-
-    window.addEventListener('load', (event) => {
-        // console.log("All resources finished loading!");
-        // document.querySelector('body').className = 'loaded';
-
-        document.querySelectorAll('pre.highlight code').forEach((el) => {
-            hljs.highlightElement(el);
-        });
-    });
+    // document.querySelectorAll('link').forEach((link) => {
+    //     if (link.rel === "preload") {
+    //         console.debug(link.href);
+    //
+    //         link.rel = "stylesheet"
+    //         link.onload = null
+    //     }
+    // });
+    //
+    // window.addEventListener('load', (event) => {
+    //     // console.log("All resources finished loading!");
+    //     // document.querySelector('body').className = 'loaded';
+    //
+    //     document.querySelectorAll('pre.highlight code').forEach((el) => {
+    //         hljs.highlightElement(el);
+    //     });
+    // });
 
     // var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     // var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
