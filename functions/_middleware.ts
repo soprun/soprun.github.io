e743ae2541db352c6ec322796d6fd30bd8fbc925
Vladislav Soprun <develop@soprun.com>
2023-06-18 17:29:16 +0300

    commit...

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -133,7 +133,7 @@ export const onRequest: PagesFunction = async ({next}) => {
 
 
     let Content_Security_Policy = "" +
-        "default-src 'self' 'unsafe-inline' *.sentry.io;" +
+        "default-src 'self' 'unsafe-inline' *;" +
         "script-src 'self' 'unsafe-inline' browser.sentry-cdn.com static.cloudflareinsights.com;" +
         "style-src 'self' 'unsafe-inline' *.soprun-github-io.pages.dev cdnjs.cloudflare.com fonts.googleapis.com;" +
         "object-src 'none';" +
@@ -144,8 +144,7 @@ export const onRequest: PagesFunction = async ({next}) => {
         "img-src 'self' https: images.unsplash.com;" +
         "manifest-src 'self';" +
         "media-src 'self';" +
-        "report-uri https://o364305.ingest.sentry.io/api/6291966/security/?sentry_key=5943bcec0a2e4787882cbb988fd0aabc;" +
-        "worker-src 'none';"
+        "report-uri https://o364305.ingest.sentry.io/api/6291966/security/?sentry_key=5943bcec0a2e4787882cbb988fd0aabc;";
 
     // response.headers.set('Content-Security-Policy', Content_Security_Policy);
     response.headers.set('Content-Security-Policy-Report-Only', Content_Security_Policy);
