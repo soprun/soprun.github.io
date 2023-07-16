9ed9a2048be38785ca63908c5c4fe21b17ce5e9f
Vladislav Soprun <develop@soprun.com>
2023-07-16 23:49:08 +0300

    commit...

--- functions/middleware.ts
+++ functions/middleware.ts
@@ -78,6 +78,8 @@ export const onRequestOptions: PagesFunction = async () => {
 // };
 
 export const onRequest: PagesFunction<Env> = async (context) => {
+    // TODO: Сломается при релизе!
+
     const response = await (context);
     const site_url = await context.env.KV.get('SITE_URL');
 
